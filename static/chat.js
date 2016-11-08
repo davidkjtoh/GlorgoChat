990e509a7a6a3113fa5c16fa4ff8cc98d6a028d5
davidkjtoh <david@imdplasia.com>
2016-11-08 16:35:49 +0800

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -6,20 +6,7 @@ angular.module('chatApp', []).controller('ChatController', function() {
     
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
-        var chat = $("#chat")
-        var ele = $('<tr></tr>')
-
-        ele.append(
-            $("<td></td>").text(data.timestamp)
-        )
-        ele.append(
-            $("<td></td>").text(data.handle)
-        )
-        ele.append(
-            $("<td></td>").text(data.message)
-        )
-        
-        chat.append(ele)
+        chatform.messages.push({text:data.timestamp, text:data.handle, text:data.message});
     };
 
     chatform.submit = function() {
