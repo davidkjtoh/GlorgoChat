3fd442ea3777b4a1ccc506cb5216e3b751c81fa0
davidkjtoh <david@imdplasia.com>
2016-11-15 16:15:26 +0800

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,7 +1,7 @@
-angular.module('chatApp', []).controller('ChatController', function($scope) {
+angular.module('chatApp', []).controller('ChatController', function($scope, $location) {
     // When we're using HTTPS, use WSS too.
-    var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new WebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+    var ws_scheme = $location.protocol() == "https:" ? "wss" : "ws";
+    var chatsock = new WebSocket(ws_scheme + '://' + $location.host() + "/chat" + $location.pathname());
     
     chatsock.onmessage = function(message) {
         var data = JSON.parse(message.data);
