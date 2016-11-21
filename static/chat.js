d8821c0e36b5f321498cb7f4806f3931cf10af64
davidkjtoh <david@imdplasia.com>
2016-11-21 13:56:59 +0800

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -1,21 +1,42 @@
-angular.module('chatApp', []).controller('ChatController', function($scope) {
+var app = angular.module('chatApp', []);
+
+app.controller('ChatController', function($scope, websocketService) {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    var chatsock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname);
+    var socket_url = ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname;
     
-    chatsock.onmessage = function(message) {
+    var ws = websocketService.start(socket_url, function (message) {
         var data = JSON.parse(message.data);
-        $scope.message = 'RECEIVED';
-        $scope.messages.push({timestamp:data.timestamp, handle:data.handle, message:data.message});
-    };
+        $scope.$apply(function () {
+            $scope.messages.push({timestamp:data.timestamp, handle:data.handle, message:data.message});
+        });
+    });
 
     $scope.submitMessage = function() {
         var message = {
             handle: $scope.handle,
             message: $scope.message,
         }
-        chatsock.send(JSON.stringify(message));
+        ws.send(JSON.stringify(message));
         $scope.message = '';
         return false;
     };
+});
+
+app.factory('websocketService', function () {
+    return {
+        start: function (url, callback) {
+            var websocket = new WebSocket(url);
+            websocket.onopen = function () {
+            };
+            websocket.onclose = function () {
+            };
+            websocket.onmessage = function (evt) {
+                callback(evt);
+            };
+            websocket.send = function (message) {
+                websocket.send(message);
+            }
+        }
+    }
 });
