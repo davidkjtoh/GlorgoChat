7c497ade8ac8388ac859d0168b4a3967baef6e48
davidkjtoh <david@imdplasia.com>
2016-11-21 18:14:14 +0800

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -23,40 +23,39 @@ app.controller('ChatController', function($scope, socket) {
 });
 
 app.factory('socket', [function() {
-    createSocket: function(url) {
-        var stack = [];
-        var onmessageDefer;
-        socket = {
-            ws: new WebSocket(url),
-            send: function(data) {
-                data = JSON.stringify(data);
-                if (socket.ws.readyState == 1) {
-                    socket.ws.send(data);
-                } else {
-                    stack.push(data);
-                }
-            },
-            onmessage: function(callback) {
-                if (socket.ws.readyState == 1) {
-                    socket.ws.onmessage = callback;
-                } else {
-                    onmessageDefer = callback;
-                }
-            }
-        };
-        socket.ws.onopen = function(event) {
-            for (i in stack) {
-                socket.ws.send(stack[i]);
-            }
-            stack = [];
-            if (onmessageDefer) {
-                socket.ws.onmessage = onmessageDefer;
-                onmessageDefer = null;
-            }
-        };
-        return socket;
-    };
     return {
-        createSocket: createSocket;
+        createSocket: function(url) {
+            var stack = [];
+            var onmessageDefer;
+            socket = {
+                ws: new WebSocket(url),
+                send: function(data) {
+                    data = JSON.stringify(data);
+                    if (socket.ws.readyState == 1) {
+                        socket.ws.send(data);
+                    } else {
+                        stack.push(data);
+                    }
+                },
+                onmessage: function(callback) {
+                    if (socket.ws.readyState == 1) {
+                        socket.ws.onmessage = callback;
+                    } else {
+                        onmessageDefer = callback;
+                    }
+                }
+            };
+            socket.ws.onopen = function(event) {
+                for (i in stack) {
+                    socket.ws.send(stack[i]);
+                }
+                stack = [];
+                if (onmessageDefer) {
+                    socket.ws.onmessage = onmessageDefer;
+                    onmessageDefer = null;
+                }
+            };
+            return socket;
+        };
     };
 }]);
