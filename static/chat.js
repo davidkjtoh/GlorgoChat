cdeb454ba47d6755e61d4eef76f34d0d4852071c
davidkjtoh <david@imdplasia.com>
2016-11-21 18:06:35 +0800

    Update chat.js

--- static/chat.js
+++ static/chat.js
@@ -4,9 +4,9 @@ app.controller('ChatController', function($scope, socket) {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
     var socket_url = ws_scheme + '://' + window.location.host + "/chat" + window.location.pathname;
-    socket.ws = new WebSocket(socket_url);
+    var web_socket = socket.createSocket(url);
     
-    socket.onmessage(function(message) {
+    web_socket.onmessage(function(message) {
         var data = JSON.parse(message.data);
         $scope.messages.push({timestamp:data.timestamp, handle:data.handle, message:data.message});
     });
@@ -16,7 +16,7 @@ app.controller('ChatController', function($scope, socket) {
             handle: $scope.handle,
             message: $scope.message,
         }
-        socket.send(message);
+        web_socket.send(message);
         $scope.message = '';
         return false;
     };
@@ -25,33 +25,35 @@ app.controller('ChatController', function($scope, socket) {
 app.factory('socket', [function() {
     var stack = [];
     var onmessageDefer;
-    socket = {
-        ws: new WebSocket(),
-        send: function(data) {
-            data = JSON.stringify(data);
-            if (socket.ws.readyState == 1) {
-                socket.ws.send(data);
-            } else {
-                stack.push(data);
+    createSocket: function(url) {
+        socket = {
+            ws: new WebSocket(url),
+            send: function(data) {
+                data = JSON.stringify(data);
+                if (socket.ws.readyState == 1) {
+                    socket.ws.send(data);
+                } else {
+                    stack.push(data);
+                }
+            },
+            onmessage: function(callback) {
+                if (socket.ws.readyState == 1) {
+                    socket.ws.onmessage = callback;
+                } else {
+                    onmessageDefer = callback;
+                }
             }
-        },
-        onmessage: function(callback) {
-            if (socket.ws.readyState == 1) {
-                socket.ws.onmessage = callback;
-            } else {
-                onmessageDefer = callback;
+        };
+        socket.ws.onopen = function(event) {
+            for (i in stack) {
+                socket.ws.send(stack[i]);
             }
-        }
+            stack = [];
+            if (onmessageDefer) {
+                socket.ws.onmessage = onmessageDefer;
+                onmessageDefer = null;
+            }
+        };
+        return socket;
     };
-    socket.ws.onopen = function(event) {
-        for (i in stack) {
-            socket.ws.send(stack[i]);
-        }
-        stack = [];
-        if (onmessageDefer) {
-            socket.ws.onmessage = onmessageDefer;
-            onmessageDefer = null;
-        }
-    };
-    return socket;
 }]);
